5fbcdaa19aa945afbb03e2e48e2a74fe938a3949
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2015-10-30 15:57:18 -0600

    Mutate the executable a bit at a time.
    Execute every version and report locations of failures.

--- src/newtrue/main.go
+++ src/newtrue/main.go
@@ -8,25 +8,30 @@ import (
 )
 
 const (
-	inpath = "./true.in"
+	inpath  = "./true.in"
 	outpath = "./true.out"
 )
 
 func main() {
-	var content []byte
-	var err error
-
-	if content, err = ioutil.ReadFile(inpath); err != nil {
+	content, err := ioutil.ReadFile(inpath)
+	if err != nil {
 		os.Exit(1)
 	}
 
-	if err = ioutil.WriteFile(outpath, content, 0777); err != nil {
-		os.Exit(1)
-	}
 
-	cmd := exec.Command(outpath)
-	if err = cmd.Run(); err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
+	for byt := range content {
+		for bit := uint32(0); bit < 8; bit++ {
+			content[byt] ^= (1 << bit)
+
+			if err = ioutil.WriteFile(outpath, content, 0777); err != nil {
+				os.Exit(1)
+			}
+
+			cmd := exec.Command(outpath)
+			content[byt] ^= (1 << bit)
+			if err = cmd.Run(); err != nil {
+				fmt.Printf("byte = %d, bit = %d\n", byt, bit)
+			}
+		}
 	}
 }
